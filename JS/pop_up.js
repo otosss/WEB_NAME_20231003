076243de04a062747e7d87a76f219f61cf71794e
admin <dll36781@naver.com>
2024-04-17 14:21:23 +0900

    리오레

--- JS/pop_up.js
+++ JS/pop_up.js
@@ -1,35 +1,36 @@
 function pop_up() {
      window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+ }
+ 
+ function show_clock(){
+     let currentDate= new Date(); // 현재시스템날짜객체생성
+     let divClock= document.getElementById('divClock');
+     let msg = "현재시간: ";
+     if(currentDate.getHours()>12){  // 12시보다크면오후아니면오전
+         msg += "오후";
+         msg += currentDate.getHours()-12+"시";
      }
-     
-function show_clock(){
-let currentDate= new Date(); // 현재시스템날짜객체생성
-let divclock= document-getElementById('divclock');
-let msg = "현재시간: ";
-if (currentDate-getHours ()>12){ // 12시보다크면오후아니면오전
-msg += "오후";
-msg += currentDate.getHours() -12+"A|";
-}
-else {
-     msg += "₽I";
-msg += currentDate.getHours()+"A|";
-}
-msg += currentdate.getMinutes()+"™";
-msg += currentDate.getSeconds()+"=";
-divclock.innerText- msg;
-
-if (currentDate-getMinutes()>58) { // 정각1분전빨강색출력
-divclock.style.color="red";
-
-setTimeout(shon_clock, 1800); //1초마다갱신
-}
-// function over (obj) {
-// obj src="image/LOGO.png*;
-// }
-}
-const over = (obj) => {
-obj. src = "image/LOGO.png";
-};
-function out (obj) {
-obj. src="image/LOGO_2-png";
+     else {
+      msg += "오전";
+      msg += currentDate.getHours()+"시";
+     }
+     msg += currentDate.getMinutes()+"분";
+     msg += currentDate.getSeconds()+"초";
+     divClock.innerText= msg;
+ 
+     if (currentDate.getMinutes()>58) {    //정각1분전빨강색출력
+       divClock.style.color="red";
+     }
+     setTimeout(show_clock, 1000);  //1초마다갱신
+ }
+ 
+ // function over(obj) {
+ //     obj.src="image/LOGO.png";
+ // }
+ const over = (obj) => {
+     obj.src = "image/LOGO.png";
  };
+ 
+ function out(obj) {
+ obj.src="image/LOGO_2.png";
+  };
