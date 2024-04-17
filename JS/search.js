8c2ab10405fe22a2d68e164b966beb3e297803b6
admin <dll36781@naver.com>
2024-04-17 13:44:31 +0900

    리오레

---
+++ JS/search.js
@@ -0,0 +1,17 @@
+document -getElementById("search_btn"). addEventListener ('click', search_message);
+
+// function search message){
+// alert("검색을 수행합니다!");
+// }
+const search_message = () => {
+const c = '검색을 수행합니다';
+alert(c);
+};
+function googleSearch() {
+const searchTerm= document.getElementById("search_input").value; // 검색어로 설정
+const googleSearchurl = 'https://www.google.com/search?g=$(encodeURIComponent(searchTerm)}';
+// 새 창에서 구글 검색을 수행
+
+window.open(googleSearchurl,"_blank"); // 새로운 창에서 열기.
+return false;
+}
